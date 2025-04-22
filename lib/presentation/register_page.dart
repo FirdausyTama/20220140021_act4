972688ab613481b5e30323169afe74342fd31da8
FirdausyTama <atama.firdausy@gmail.com>
2025-04-22 18:54:45 +0700

    membuat textform field untuk register

--- lib/presentation/register_page.dart
+++ lib/presentation/register_page.dart
@@ -8,8 +8,64 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+ final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final _formKey = GlobalKey<FormState>();
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Register'),
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: passwordController,
+                decoration: const InputDecoration(labelText: 'Password'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
+              ElevatedButton(
+                onPressed: (){},
+                child: Text('Register')
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pushNamed( context, '/login');
+                  // Navigator.pop(context);
+                  // Navigator.push(
+                  //  context,
+                  //  MaterialPageRoute(builder: (context) => const LoginPage()),
+                  //);
+                },
+                child: Text('Sudah punya akun? Login Sekarang!'),
+                )
+            ],
+          ))
+      ),
+    );
   }
 }
